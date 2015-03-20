c89d3c66420e7b5a5b900d9d6496496075dbe565
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-03-20 09:34:35 +0000

    Continue playing with adding constraints to zoomed case

--- constrainedzoom.py
+++ constrainedzoom.py
@@ -4,13 +4,25 @@ import math
 import scipy.fftpack
 import pylab as p
 
-def cov(x):
-    #return 1.
+def cov(x,plaw = -1.0):
+    if plaw ==0.0:
+        return 1.0
     delta = x[1]-x[0]
-    cv = x**0.5-(x-delta)**0.5
+    if plaw!=-1 :
+        cv = abs(x**(plaw+1)-(x-delta)**(plaw+1))
+    else:
+        cv = np.log(x)-np.log(x-delta)
     cv[cv==np.inf]=0
     cv[cv!=cv]=0
     return cv
+
+def Ufft(x):
+    """Unitary FFT"""
+    return scipy.fftpack.rfft(x)/math.sqrt(float(len(x)))
+
+def Uifft(x):
+    """Unitary inverse FFT"""
+    return scipy.fftpack.irfft(x)*math.sqrt(float(len(x)))
 
 class ZoomConstrained(object):
     def __init__(self, cov_fn = cov, n1=256, n2=768, scale=4, offset = 10):
@@ -19,7 +31,17 @@ class ZoomConstrained(object):
         self.n2 = n2
         self.scale = scale
         self.offset = offset
-        self.k_cut = self.n1*0.1
+        self.k_cut = self.n1*0.05
+        self.weights = self.interpolation_coeffs()
+        delta_low = 1./self.n1
+        delta_high = 1./(self.n2*self.scale)
+        self.k_low = scipy.fftpack.rfftfreq(self.n1,d=delta_low)
+        self.k_high = scipy.fftpack.rfftfreq(self.n2,d=delta_high)
+        self.C_low = self.cov_fn(self.k_low)
+        self.C_high = self.cov_fn(self.k_high)
+        self.zoom_fac = self.scale*(self.n2/self.n1)
+        self.constraints =[]
+        self.constraints_val = []
 
     def filter_low(self, k):
         return k<self.k_cut
@@ -40,6 +62,48 @@ class ZoomConstrained(object):
                            self.offset+self.n1/self.scale-delta_fine,self.n2)
 
 
+    def lowres_covariance(self):
+        # calculate the pixel-space diagonal variance and just-off-diagonal covariance
+        # for the lo-res grid
+        k = scipy.fftpack.rfftfreq(self.n1,d=1./self.n1)
+        C = self.cov_fn(k)
+
+        pa = np.zeros(self.n1)
+        pb = np.zeros(self.n1)
+        pa[0]=1
+        pb[1]=1
+
+        pa_k = Ufft(pa)
+        pb_k = Ufft(pb)
+
+        covm = np.zeros((2,2))
+
+        covm[0,0] = np.dot(pa_k,C*pa_k)
+        covm[0,1] = np.dot(pa_k,C*pb_k)
+        covm[1,0] = covm[0,1]
+        covm[1,1] = covm[0,0]
+
+        return covm
+
+    def interpolation_coeffs(self):
+        grid_scalefactor = (self.scale*self.n2)/self.n1
+
+        C = self.lowres_covariance()
+        X = C[1,0]/C[0,0]
+
+        # following is exact for X=0
+        weights = (np.arange(grid_scalefactor+1,dtype=float)/grid_scalefactor)**0.5
+
+        print X
+        for i in range(10):
+            xweights = weights
+            weights = -weights[::-1] * X + np.sqrt(weights[::-1]**2*(X**2-1)+1)
+            weights = (weights+xweights)/2
+
+
+        return np.arange(grid_scalefactor+1,dtype=float)/grid_scalefactor
+
+
     def realization(self):
 
         grid_scalefactor = (self.scale*self.n2)/self.n1
@@ -55,31 +119,26 @@ class ZoomConstrained(object):
         delta_high_k = self.randoms(k_high)
 
         delta_high_k*=self.filter_high(k_high)
+        delta_low_k_plus = delta_low_k+self.filter_high(k_low) # store the filtered-out components of the low-res field
+        delta_low_k*=self.filter_low(k_low)
 
-        delta_low = scipy.fftpack.irfft(delta_low_k*self.filter_low(k_low))
-        delta_low2 = scipy.fftpack.irfft(delta_low_k)
-        delta_high = scipy.fftpack.irfft(delta_high_k)
 
-        # now interpolate the low frequency contribution into the high-res window
-        # prepare the data range just around the window ...
+        for (al_low,al_high), d in zip(self.constraints, self.constraints_val):
+            scale = d - np.dot(al_low,delta_low_k) - np.dot(al_high, delta_high_k)
+            delta_low_k += self.C_low*al_low * scale
+            delta_high_k += self.C_high*al_high  * scale
+            print np.dot(al_low,delta_low_k)+np.dot(al_high,delta_high_k),d
 
-        delta_low_window = delta_low[self.offset-1:self.offset+self.n1/self.scale+1]
-        delta_lowhigh = np.zeros(self.n2+grid_scalefactor)
+        delta_low, delta_high = self.harmonic_to_pixel(delta_low_k,
+                                                       delta_high_k)
 
-        #  ... and interpolate linearly ...
 
-        for i in range(grid_scalefactor):
-            weight_hi = float(i)/grid_scalefactor
-            weight_lo = 1.0-weight_hi
-            delta_lowhigh[i::grid_scalefactor] = weight_lo*delta_low_window[:-1] +\
-                weight_hi*delta_low_window[1:]
 
-        # trim to size
-        upper = -grid_scalefactor/2+1
-        if upper==0: upper = None
-        delta_lowhigh = delta_lowhigh[grid_scalefactor/2+1:upper]
 
-        return delta_low2, delta_high+delta_lowhigh
+        # not sure how good this is - but add in the high-k power into the low-res region
+        delta_low2 = 1.0*Uifft(delta_low_k_plus)
+
+        return delta_low2+delta_low, delta_high
 
     def estimate_cov(self, Ntrials=2000):
         cov = np.zeros((self.n1+self.n2,self.n1+self.n2))
@@ -92,22 +151,73 @@ class ZoomConstrained(object):
         cov[self.n1:,:self.n1]=cov[:self.n1,self.n1:].T
         return cov
 
+    def harmonic_to_pixel(self, f_low_k, f_high_k):
+        delta_low = Uifft(f_low_k*self.filter_low(self.k_low))
+        delta_high = Uifft(f_high_k)
 
-def demo():
-    #np.random.seed(1)
-    G = ZoomConstrained()
+        #  interpolate the low frequency contribution into the high-res window
+        # prepare the data range just around the window ...
 
-    x0, x1 = G.xs()
-    r0,r1 = G.realization()
-    p.plot(x0,r0)
-    p.plot(x1,r1)
+        delta_low_window = delta_low[self.offset-1:self.offset+self.n1/self.scale+1]
+        delta_lowhigh = np.zeros(self.n2+self.zoom_fac)
+
+        #  ... and interpolate linearly ...
+
+        for i in range(self.zoom_fac):
+            weight_hi = self.weights[i]
+            weight_lo = self.weights[self.zoom_fac-i]
+            delta_lowhigh[i::self.zoom_fac] = weight_lo*delta_low_window[:-1] +\
+                weight_hi*delta_low_window[1:]
+
+        # trim to size
+        upper = -self.zoom_fac/2+1
+        if upper==0: upper = None
+        delta_lowhigh = delta_lowhigh[self.zoom_fac/2+1:upper]
+
+        return delta_low, delta_lowhigh+delta_high
+
+    def hr_pixel_to_harmonic(self, vec=None):
+        if vec is None:
+            vec = np.zeros(self.n2)
+            vec[self.n2/2]=1.0
+
+        # FA . vec
+        vec_k_high = Ufft(vec)
+
+        # down-sample
+        vec_lr = np.zeros(self.n1)
+        vec_lr[self.offset:self.offset+self.n2/self.zoom_fac] = \
+              vec.reshape((self.n2/self.zoom_fac,self.zoom_fac)).mean(axis=1)
+
+        vec_k_low = Ufft(vec_lr)
+
+        return vec_k_low*self.filter_low(self.k_low), \
+               vec_k_high*self.filter_high(self.k_high)
+
+        # FB . vec
+
+    def norm(self, low, high):
+        return np.dot(low,low*self.C_low)+np.dot(high,high*self.C_high)
+
+    def xCy(self, low1, high1, low2, high2):
+        return np.dot(low1,low2*self.C_low)+np.dot(high1,high2*self.c_high)
+
+    def add_constraint(self, val=0.0, hr_vec=None):
+        if len(self.constraints)>0:
+            print "oops - G-S not implemented yet for multiple constraints"
+
+        low, high = self.hr_pixel_to_harmonic(hr_vec)
+        norm = self.norm(low,high)
+        low/=math.sqrt(norm)
+        high/=math.sqrt(norm)
+        val/=math.sqrt(norm)
+
+        self.constraints.append((low,high))
+        self.constraints_val.append(val)
 
 
-def cov_demo():
-    G = ZoomConstrained()
 
-    cov = G.estimate_cov()
-
+def display_cov(G, cov, downgrade=False):
     vmin = np.min(cov)
     vmax = np.max(cov)
 
@@ -118,6 +228,15 @@ def cov_demo():
     zoom_width = G.n1/G.scale
 
     p.imshow(C11,extent=[0,G.n1,G.n1,0],vmin=vmin,vmax=vmax,interpolation='nearest')
+    if downgrade:
+        zoom_fac = G.scale*(G.n2/G.n1)
+        print "zoom_fac=",zoom_fac
+        C22new=0
+        for i in range(zoom_fac):
+            for j in range(zoom_fac):
+                C22new += C22[i::zoom_fac,j::zoom_fac]
+        C22 = C22new/zoom_fac**2
+
     p.imshow(C12.T,extent=[0,G.n1,G.offset+zoom_width,G.offset],vmin=vmin,vmax=vmax,interpolation='nearest')
     p.imshow(C22,extent=[G.offset,G.offset+zoom_width,G.offset+zoom_width,G.offset],vmin=vmin,vmax=vmax,interpolation='nearest')
 
@@ -128,3 +247,20 @@ def cov_demo():
 
     p.xlim(0,G.n1)
     p.ylim(G.n1,0)
+
+
+def demo(val=2.0):
+    np.random.seed(1)
+    G = ZoomConstrained()
+    G.add_constraint(val)
+    x0, x1 = G.xs()
+    r0, r1 = G.realization()
+    p.plot(x0,r0)
+    p.plot(x1,r1)
+
+
+def cov_demo():
+    G = ZoomConstrained()
+
+    cov = G.estimate_cov()
+    display_cov(G, cov)
--- multiscale.py
+++ multiscale.py
@@ -5,6 +5,7 @@ import scipy.fftpack
 import pylab as p
 
 def cov(x):
+    return 1
     cv = x**-1.0
     cv[cv==np.inf]=0
     cv[cv!=cv]=0
@@ -12,10 +13,12 @@ def cov(x):
 
 def filt(x,level=0,maxlevel=0):
     f = np.ones_like(x)
+
     if level<maxlevel:
-        f[len(f)/4:]=0
+        f[len(f)/2:]=0
     if level>0:
-        f[:len(f)/8] = 0
+        f[:len(f)/4] = 0
+
     return f
 
 class MultiscaleGaussian(object):
@@ -36,15 +39,18 @@ class MultiscaleGaussian(object):
         r = scipy.fftpack.irfft(r)
 
         if means is not None:
-            means/=np.sqrt(2)
-            means = means[:self.size/2]
-            means_expanded = np.zeros(self.size)
-            means_expanded[::2] = means
-            means_expanded[1::2] = means
+            #means/=np.sqrt(2)
 
-            means_expanded = scipy.fftpack.rfft(means_expanded)
-            means_expanded*=(1.-filt(r,level,self.n_refine))
-            means_expanded = scipy.fftpack.irfft(means_expanded)
+            means_c = np.concatenate([[means[-1]],means[:self.size/2+1]])
+            means_c = means[:self.size/2+2]
+
+            means_expanded = np.zeros(self.size)
+            means_expanded[::2] = 2*means_c[1:-1]/3+means_c[:-2]/3
+            means_expanded[1::2] = 2*means_c[1:-1]/3+means_c[2:]/3
+
+            #means_expanded = scipy.fftpack.rfft(means_expanded)
+            #means_expanded*=(1.-filt(r,level,self.n_refine))
+            #means_expanded = scipy.fftpack.irfft(means_expanded)
 
             r+=means_expanded
 
@@ -61,15 +67,26 @@ class MultiscaleGaussian(object):
         return xs
 
     def estimate_cov(self, level, Ntrials=4000):
-        print scipy.fftpack.rfftfreq(self.size,d=2.0**level)
         cov = np.zeros((self.size,self.size))
         for i in xrange(Ntrials):
             r = self.realization()[level]
             cov+=np.outer(r,r)
         return cov
 
+    def estimate_covs(self, Ntrials=4000):
+        cov = np.zeros(((self.size*(self.n_refine+1),(self.size*(self.n_refine+10)))))
+
+        for i in xrange(Ntrials):
+            r = self.realization()
+            for l1 in range(self.n_refine+1):
+                for l2 in range(self.n_refine+1):
+                    cov[l1*self.size:(l1+1)*self.size,l2*self.size:(l2+1)*self.size]+=np.outer(r[l1],r[l2])
+
+        return cov
+
 
 def demo():
+    np.random.seed(1)
     G = MultiscaleGaussian(cov, 1)
 
     G2 = MultiscaleGaussian(cov, 0,512)
@@ -82,6 +99,41 @@ def demo():
     x0 = G2.xs()[0]/2
     r0, = G2.realization()
     p.plot(x0,r0)
+
+def display_multicov(covs, base_size=256, vmin=None, vmax=None, base_extent=None):
+    if not vmin:
+        vmin = covs.min()
+        vmax = covs.max()
+
+    nrefine = len(covs)/base_size
+    assert nrefine*base_size==len(covs)
+
+    if not base_extent:
+        base_extent = base_size*2**nrefine
+
+
+    # the base layer
+    print "display",base_extent
+    p.imshow(covs[:base_size,:base_size],extent=[0,base_extent,base_extent,0],vmin=vmin,vmax=vmax)
+    p.plot([0,base_extent,base_extent,0,0],[0,0,base_extent,base_extent,0],'w-')
+
+    if nrefine>1:
+        # diagonals between the refined regions and the current region
+        p.imshow(covs[base_size/2:base_size,base_size:2*base_size],
+                 extent=[0,base_extent/2,base_extent,base_extent/2],vmin=vmin,vmax=vmax)
+        p.imshow(covs[base_size:2*base_size,base_size/2:base_size],
+                 extent=[base_extent/2,base_extent,base_extent/2,0],vmin=vmin,vmax=vmax)
+
+
+
+        # the refined region
+        display_multicov(covs[base_size:,base_size:],base_size,vmin=vmin,vmax=vmax,base_extent=base_extent/2)
+
+        p.plot([base_extent/2,base_extent],[base_extent/2,base_extent/2],"w:")
+        p.plot([base_extent/2,base_extent/2],[base_extent/2,base_extent],'w:')
+
+        p.xlim(0,base_extent)
+        p.ylim(base_extent,0)
 
 def demo_cov(base_size=128,nrefine=3):
     G = MultiscaleGaussian(cov, nrefine, size=base_size)
@@ -100,3 +152,12 @@ def demo_cov(base_size=128,nrefine=3):
 
     p.subplot(133)
     p.imshow(cv1-cv2[:256,:256],vmin=-vmax,vmax=vmax)
+
+def demo_multicov(base_size=128,nrefine=3):
+    covs = MultiscaleGaussian(cov, nrefine, size=base_size).estimate_covs()
+    p.subplot(121)
+    vmin = covs.min()
+    vmax = covs.max()
+    display_multicov(covs,base_size)
+    p.subplot(122)
+    p.imshow(covs[:base_size,:base_size],vmin=vmin,vmax=vmax)
