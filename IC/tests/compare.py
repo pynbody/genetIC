f3e35382bd46b6b3015b0213ad7c73f9b2400e19
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2015-11-29 18:45:24 +0000

    Fix reference files for tests following earlier tipsy units error

--- IC/tests/compare.py
+++ IC/tests/compare.py
@@ -6,7 +6,7 @@ import pynbody
 import sys
 
 def compare(f1,f2) :
-    npt.assert_almost_equal(f1['vel'],f2['vel'],decimal=3)
+    npt.assert_almost_equal(f1['vel'],f2['vel'],decimal=4)
     npt.assert_almost_equal(f1['pos'],f2['pos'],decimal=4)
     npt.assert_almost_equal(f1['mass'],f2['mass'],decimal=6)
     #npt.assert_almost_equal(f1['eps'],f2['eps'])
