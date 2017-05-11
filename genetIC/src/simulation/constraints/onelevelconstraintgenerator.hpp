b05dc2728c9227c26b414f5fe6e198cfcd5e560c
Martin-Rey <mrey@imac7.star.ucl.ac.uk>
2017-05-11 15:56:14 +0100

    Reversing changes done uncarefully done on master

--- genetIC/src/simulation/constraints/onelevelconstraintgenerator.hpp
+++ genetIC/src/simulation/constraints/onelevelconstraintgenerator.hpp
@@ -165,10 +165,6 @@ namespace constraints {
 
     }
 
-    void variance(){
-
-    }
-
     fields::Field<DataType, FloatType> getResult() {
       return std::move(outputField);
     };
@@ -195,8 +191,6 @@ namespace constraints {
       calc.angmom(1);
     } else if (name == "lz") {
       calc.angmom(2);
-    } else if (name == "variance") {
-      calc.variance();
     } else {
       throw std::runtime_error("Unknown constraint type " + name);
     }
