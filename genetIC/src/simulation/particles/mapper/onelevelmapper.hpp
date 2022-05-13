bb13592a9309ca65226d3987a5e5a02b3ca1ef2a
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2022-05-13 16:27:41 +0100

    Fix bug in new splitMass interpretation where mass went missing on levels to which no gas is added

--- genetIC/src/simulation/particles/mapper/onelevelmapper.hpp
+++ genetIC/src/simulation/particles/mapper/onelevelmapper.hpp
@@ -153,16 +153,20 @@ namespace particle {
         \param toGrids - vector of grids - only create usable mappers if this grid is a proxy for one of these.
       */
       std::pair<MapPtrType, MapPtrType> splitMass(T massRatio, const std::vector<GridPtrType> &toGrids) override {
-        auto s1 = std::make_shared<OneLevelParticleMapper<GridDataType>>(
-          this->pGrid->makeScaledMassVersion(massRatio));
-        auto s2 = std::make_shared<OneLevelParticleMapper<GridDataType>>(
-          this->pGrid->makeScaledMassVersion(1.0 - massRatio));
+        std::shared_ptr<OneLevelParticleMapper<GridDataType>> s1,s2;
 
-        s1->setGadgetParticleType(0);
-        s2->setGadgetParticleType(this->gadgetParticleType);
-
-        if(!pGrid->isProxyForAnyOf(toGrids))
-          s1 = std::shared_ptr<OneLevelParticleMapper<GridDataType>>(nullptr);
+        if(pGrid->isProxyForAnyOf(toGrids)) {
+          s1 = std::make_shared<OneLevelParticleMapper<GridDataType>>(
+            this->pGrid->makeScaledMassVersion(massRatio));
+          s2 = std::make_shared<OneLevelParticleMapper<GridDataType>>(
+            this->pGrid->makeScaledMassVersion(1.0 - massRatio));
+          s1->setGadgetParticleType(0);
+          s2->setGadgetParticleType(this->gadgetParticleType);
+        } else {
+          s1 = nullptr;
+          s2 = std::make_shared<OneLevelParticleMapper<GridDataType>>(this->pGrid);
+          s2->setGadgetParticleType(this->gadgetParticleType);
+        }
 
         return std::make_pair(s1, s2);
 
